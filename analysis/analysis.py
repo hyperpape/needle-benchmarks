5ad91fb78c8f95f92e55fdef90d5973a3c7fa457
Justin Blank <justin.blank@gmail.com>
2021-07-10 23:00:58 -0400

    Add categorization to analysis

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -4,67 +4,111 @@ from collections import defaultdict
 from functools import reduce
 import json
 from operator import mul
+import re
 import sys
+
+SPECIAL_CHARS = re.compile(r"[|*{}+]")
+
 
 def collect_regex_benchmarks(filename):
     data = read_file(filename)
     batches = defaultdict(dict)
     for result in data:
-        score = result['primaryMetric']['score']
-        test = result['benchmark'].split('.')[-1]
-        if not 'params' in result or not 'regexString' in result['params']:
+        score = result["primaryMetric"]["score"]
+        test = result["benchmark"].split(".")[-1]
+        if not "params" in result or not "regexString" in result["params"]:
             continue
-        batches[result['params']['regexString']][test] = score
+        regex_string = result["params"]["regexString"]
+        batches[regex_string][test] = score
+        batches[regex_string]["category"] = categorize(regex_string)
     return batches
+
 
 def java_pattern_comparison(data):
     ratio = 1
     count = data
-    ratios = [v['javaRegex'] / v['pattern'] for v in data.values()]
+    key_set = defaultdict(list)
+    for k in data.keys():
+        key_set[categorize(k)].append(k)
+    comparisons = {}
+    for key in key_set:
+        comparisons[key] = compare_keys(data, key_set[key])
+    return comparisons
+
+
+def compare_keys(data, keys):
+    ratios = [data[k]["javaRegex"] / data[k]["pattern"] for k in keys]
     ratio = reduce(mul, ratios, 1)
-    geomean = ratio ** (1 / len(data))
+    geomean = ratio ** (1 / len(keys))
     best = max(ratios)
     worst = min(ratios)
-    return {
-        'geomean': geomean,
-        'best': best,
-        'worst': worst
-    }
+    return {"geomean": geomean, "best": best, "worst": worst}
+
 
 def collated(data):
     collated = {}
     for result in data:
-        key = result['benchmark'] + ':' + result['params']['regexString']
-        collated[key] = result
+        try:
+            key = result["benchmark"] + ":" + result["params"]["regexString"]
+            collated[key] = result
+        except Exception:
+            pass
     return collated
 
-def compare_runs(loc1, loc2):
+
+def compare_runs(loc1, loc2, threshold=0.01, no_java=False):
     data1 = collated(read_file(loc1))
     data2 = collated(read_file(loc2))
     ratio = 1
-    left_better = 0
-    right_better = 0
+    left_better = []
+    right_better = []
     for k, result in data1.items():
-        score = result['primaryMetric']['score']
-        test = result['benchmark'].split('.')[-1]
-        difference = score / data2[k]['primaryMetric']['score']
-        print(result['benchmark'] + ':' + str(difference))
-        if difference > 1.01:
-            right_better += 1
-        elif difference < .99:
-            left_better += 1
+        if no_java and "java" in result["benchmark"]:
+            continue
+
+        score = result["primaryMetric"]["score"]
+        test = result["benchmark"].split(".")[-1]
+
+        key = test
+        if "regexString" in result["params"]:
+            regexString = result["params"]["regexString"]
+            key += f"({regexString})"
+
+        difference = score / data2[k]["primaryMetric"]["score"]
+        cmp = {"key": key, "difference": difference, "category": categorize(key)}
+        if difference > 1 + threshold:
+            right_better.append(cmp)
+        elif difference < (1 / (1 + threshold)):
+            left_better.append(cmp)
         ratio *= difference
     return {
-        'count': count,
-        'geomean': ratio ** (1 / len(data)),
-        'left_better': left_better,
-        'right_better': right_better
+        "count": len(data1),
+        "geomean": ratio ** (1 / len(data1)),
+        "left_better": left_better,
+        "right_better": right_better,
     }
+
+
+def categorize(key):
+    if key.startswith("pattern(") and key.endswith(")"):
+        key = key[len("pattern(") : -1]
+    if SPECIAL_CHARS.search(key):
+        if not SPECIAL_CHARS.search(key.replace("|", "")):
+            return "ALTERNATION"
+        return "ARBITRARY"
+    return "LITERAL"
+
 
 def read_file(filename):
     with open(filename) as f:
         return json.loads(f.read())
 
-if __name__ == '__main__':
-    print(java_pattern_comparison(collect_regex_benchmarks(sys.argv[1])))
-    print(collect_regex_benchmarks(sys.argv[1]))
+
+if __name__ == "__main__":
+    if sys.argv[1] == "--compare":
+        print(compare_runs(sys.argv[2], sys.argv[3], 0.05, True))
+    else:
+        for k, v in java_pattern_comparison(
+            collect_regex_benchmarks(sys.argv[1])
+        ).items():
+            print(k, v)
--- benchshort.sh
+++ benchshort.sh
@@ -3,4 +3,4 @@ then
     PROF="-prof $2"
 fi
 
-/usr/java/jdk-11/bin/java -jar target/benchmarks.jar -rf json $1 -wi 1 -i 1 -r 10 -f 1 $1 $PROF > output/$1$(date +%s) 2>&1
+/usr/java/jdk-11/bin/java -jar target/benchmarks.jar -rf json $@ -wi 1 -i 1 -r 10 -f 1 $1 $PROF > output/$1$(date +%s) 2>&1
